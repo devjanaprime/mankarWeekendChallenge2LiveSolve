4d2aa6fedf2061387227657684b4e21b6478a0cc
Dev Jana <dev@primeacademy.io>
2018-05-29 09:57:26 -0500

    after lecture - stretch goal

--- server/modules/math.js
+++ server/modules/math.js
@@ -1,9 +1,18 @@
 let express = require( 'express' );
 let router = express.Router();
 
+// history of all calculations
+let history = [];
+
+router.get( '/', ( req, res )=>{
+    res.send( history );
+}) // end GET
+
 router.post( '/', ( req, res )=>{
  console.log( 'in /math POST:', req.body );
  let answer = 0;
+ // push this into history
+ history.push( req.body );
  // do math
  switch( req.body.operator ){
     case '+': 
@@ -20,7 +29,8 @@ router.post( '/', ( req, res )=>{
         break;
  } // end switch case
  console.log( 'answer:', answer );
- res.send( 'woof' );
+ res.send( { answer: answer } );
 })// end POST
+
 
 module.exports = router;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,6 +7,8 @@ class Calculation{
 } // end class
 
 let selectedOperator = '';
+let x = '';
+let y = '';
 
 $( document ).ready( readyNow );
 
@@ -14,18 +16,22 @@ function readyNow(){
   $( '#clearButton' ).on( 'click', clear );
   $( '#equalsButton' ).on( 'click', equals );
   $( '.operatorButton' ).on( 'click', operator );
+  $( '.numberButton' ).on( 'click', number );
+  getHistory();
 } //end readyNow
 
 function clear(){
   console.log( 'in clear' );
-  $( '#num0In' ).val('');
-  $( '#num1In' ).val('');
+  x='';
+  y='';
+  selectedOperator='';
+  updateInput();
 }
 
 function equals(){
   console.log( 'in equals' );
   // get user inputs for number combine with selected operator create new Calculation
-  let calcToSend = new Calculation( $( '#num0In' ).val(), $( '#num1In' ).val(), selectedOperator );
+  let calcToSend = new Calculation( x, y, selectedOperator );
   console.log( 'sending:', calcToSend );
   // send to server via post
   $.ajax({
@@ -35,12 +41,49 @@ function equals(){
   }).then( function( response ){
     console.log( 'back from server with:', response );
     // update DOM with answer 
+    let el = $( '#answerOut' );
+    el.empty();
+    el.append( response.answer );
     // update DOM with history
+    getHistory();
   }) //end ajax
 }
+
+function getHistory(){
+  // get call to server
+  $.ajax({
+    method: 'GET',
+    url: '/math'
+  }).then( function( response ){
+    console.log( response );
+    let el = $( '#historyOut' );
+    el.empty();
+    // loop through response
+    for( calc of response ){
+      // display each in ul on DOM
+      el.append( '<li>' + calc.x + ' ' + calc.operator + ' ' + calc.y +'</li>' );
+    }
+  }) // end ajax
+}
+
+function number(){
+  console.log( 'number:', $( this ).text() );
+  if( selectedOperator === '' ){
+    x += $( this ).text();
+  } //end x
+  else{
+    y += $( this ).text();
+  }
+  updateInput();
+} //end funk
 
 function operator(){
   console.log( 'in operator:', $( this ).text() );
   selectedOperator = $( this ).text();
   console.log( 'selected operator:', selectedOperator );
+  updateInput();
+}
+
+function updateInput(){
+  $( '#numberOut' ).val( x + selectedOperator + y );
 }
